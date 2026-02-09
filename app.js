c21b04976462811df53db6c8a24d71417d5d2e4a
k-dotsingh <kishansingh2882004@gmail.com>
2026-02-09 11:47:06 +0530

    some changes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
-import connectDb from './config/db.js'
+dotenv.config()
+import { connectDb } from './config/db.js'
 
 const app = express()
 
+connectDb()
 app.use(express.json())
 
 app.get('/', (req, res) => {
+    console.log(`Server running on port ${process.env.PORT}`)
     res.send('Hello World!')
 })
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
-
-connectDb()
